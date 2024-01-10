b32712f8cb4650fab9b6f3c9258592d0b9d95fa7
powerslam <timsort@kookmin.ac.kr>
2024-01-10 17:35:29 +0900

    라바콘은 특징점이 잘 추출되지 않아서 색상 기반 추출을 함

--- src/descripter_test/scripts/descriptor.py
+++ src/descripter_test/scripts/descriptor.py
@@ -4,46 +4,48 @@ import numpy as np
 # 라바콘의 경우 이런 식으로 구분 가능함
 # 그런데 자전거 탄 사람은 어떻게?
 # 그리고 영상보면 다른 자동차도 인식해야 함
-PATH = './img/'
-src = cv2.imread(PATH + 'cone4.png')
-hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
 
-o_lo = np.array([0, 60, 60])
-o_hi = np.array([20, 255, 255])
+# 이미지 로드 (여기서는 예시 이미지로 로드)
+image = cv2.imread('cone2.png')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-o_img = cv2.inRange(hsv, o_lo, o_hi)
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-cv2.imshow('img', src)
-cv2.imshow('orange', o_img)
+o_lo = np.array([0, 190, 30])
+o_hi = np.array([15, 255, 255])
+
+r_lo = np.array([0, 190, 100])
+r_hi = np.array([15, 255, 255])
+
+o_mask = cv2.inRange(hsv, o_lo, o_hi)
+r_mask = cv2.inRange(hsv, r_lo, r_hi)
+
+# 비스무리한 빨간 영역(신호등 적색 신호)은 제거하고, 주황 영역만 구하는 코드
+rev_red = cv2.bitwise_and(gray, gray, mask=cv2.bitwise_not(r_mask))
+orange = cv2.bitwise_and(rev_red, rev_red, mask=o_mask)
+
+# 찌꺼지 제거 후 빈칸 채우기 - 필요하지 않을 수도 있음
+k = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+erode = cv2.morphologyEx(orange, cv2.MORPH_ERODE, k)
+k = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+cone = cv2.morphologyEx(erode, cv2.MORPH_DILATE, k)
+
+cone[cone > 0] = 255
+area_idx = np.where(cone > 0)
+y1, y2 = np.min(area_idx[0]), np.max(area_idx[0])
+x1, x2 = np.min(area_idx[1]), np.max(area_idx[1])
+
+y1 = max(y1 - 10, 0)
+y2 = min(y2 + 30, image.shape[0])
+
+x1 = max(x1 - 40, 0)
+x2 = min(x2 + 35, image.shape[1])
+
+roi = image[y1:y2, x1:x2]
+cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+cv2.imshow('roi', roi)
+cv2.imshow('image', image)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-# orb = cv2.ORB_create(
-#     nfeatures=1000,
-#     scaleFactor=2,
-#     nlevels=8,
-#     edgeThreshold=31,
-#     firstLevel=0,
-#     WTA_K=2,
-#     scoreType=cv2.ORB_FAST_SCORE,
-#     patchSize=31,
-#     fastThreshold=20,
-# )
-
-# kp1, des1 = orb.detectAndCompute(gray, None)
-# kp2, des2 = orb.detectAndCompute(target, None)
-
-# # 해밍 거리를 가지고 비교
-# bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-# matches = bf.match(des1, des2)
-# matches = sorted(matches, key=lambda x: x.distance)
-
-# for i in matches[:100]:
-#     idx = i.queryIdx
-#     x1, y1 = kp1[idx].pt
-#     cv2.circle(src, (int(x1), int(y1)), 3, (255, 0, 0), 1)
-
-# cv2.imshow("src", src)
-# cv2.imshow('target', gray)
-# cv2.imshow('target', target)
-# cv2.waitKey()
--- src/descripter_test/scripts/main.py
+++ src/descripter_test/scripts/main.py
@@ -15,44 +15,65 @@ class DescriptorTest():
     def __init__(self):
         rospy.init_node('descriptor_test') 
         rospy.Subscriber('/image_jpeg/compressed', CompressedImage, self.func)
-        
-        self.orb = cv2.ORB_create(
-            nfeatures=40000,
-            scaleFactor=1.2,
-            nlevels=8,
-            edgeThreshold=31,
-            firstLevel=0,
-            WTA_K=2,
-            scoreType=cv2.ORB_HARRIS_SCORE,
-            patchSize=31,
-            fastThreshold=20,
-        )
 
         self.img = None
-        self.cone = cv2.imread(PATH + 'cone4.png')
-        self.kp1, self.des1 = self.orb.detectAndCompute(self.cone, None)
-
         self.bridge = CvBridge()
         self.get_image = lambda msg: self.bridge.compressed_imgmsg_to_cv2(msg)
 
+    # 근데 아마 장애물 종류에 따라서 함수가 바뀌어야 함!!! 이게 좀 큰 문제임
     def func(self, msg: CompressedImage):
         self.img = self.get_image(msg)
-        self.matching()
+        cx1, cx2, cy1, cy2 = self.find_cone()
+        
+        cone = self.img[cy1:cy2, cx1:cx2]
 
-        cv2.imshow('img', self.img)
+        cv2.rectangle(self.img, (cx1, cy1), (cx2, cy2), (0, 255, 0), 2)
+        cv2.imshow('image', self.img)
+        cv2.imshow('cone', cone)
+        
         cv2.waitKey(1)
 
-    def matching(self):
-        _, des2 = self.orb.detectAndCompute(self.img, None)
+    def find_cone(self):
+        h, w, _ = self.img.shape
 
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        matches = bf.match(self.des1, des2)
-        matches = sorted(matches, key=lambda x: x.distance)
+        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
 
-        for i in matches[:100]:
-            idx = i.queryIdx
-            x1, y1 = self.kp1[idx].pt
-            cv2.circle(self.img, (int(x1), int(y1)), 3, (255, 0, 0), 3)
+        # 주황색 영역에 대한 색상범위
+        o_lo = np.array([0, 190, 30])
+        o_hi = np.array([15, 255, 255])
+
+        # 빨강색 영역에 대한 색상범위
+        r_lo = np.array([0, 190, 100])
+        r_hi = np.array([15, 255, 255])
+
+        # 색상범위에 대한 mask 값
+        o_mask = cv2.inRange(hsv, o_lo, o_hi)
+        r_mask = cv2.inRange(hsv, r_lo, r_hi)
+
+        # 비스무리한 빨간 영역(신호등 적색 신호)은 제거하고, 주황 영역만 구하는 코드
+        rev_red = cv2.bitwise_and(gray, gray, mask=cv2.bitwise_not(r_mask))
+        cone = cv2.bitwise_and(rev_red, rev_red, mask=o_mask)
+
+        # 찌꺼지 제거 후 빈칸 채우기 - 필요하지 않을 수도 있음
+        k = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+        erode = cv2.morphologyEx(cone, cv2.MORPH_ERODE, k)
+        
+        k = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+        cone = cv2.morphologyEx(erode, cv2.MORPH_DILATE, k) # 최종 출력
+
+        cone[cone > 0] = 255
+        area_idx = np.where(cone > 0)
+        y1, y2 = np.min(area_idx[0]), np.max(area_idx[0])
+        x1, x2 = np.min(area_idx[1]), np.max(area_idx[1])
+
+        y1 = max(y1 - 10, 0)
+        y2 = min(y2 + 30, h)
+
+        x1 = max(x1 - 40, 0)
+        x2 = min(x2 + 35, w)
+
+        return x1, x2, y1, y2
 
 if __name__ == '__main__':
     try:
