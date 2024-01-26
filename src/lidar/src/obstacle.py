54696d00e3972fb034f879b00bc3e0246f8ff29a
powerslam <timsort@kookmin.ac.kr>
2024-01-26 18:09:57 +0900

    이름 변경 및 코드 다듬기

--- src/lidar/src/obstacle.py
+++ src/lidar/src/obstacle.py
@@ -1,41 +1,75 @@
-import numpy as np
-from sklearn.cluster import DBSCAN
-from sensor_msgs.msg import PointCloud2
 import rospy
-import sensor_msgs.point_cloud2 as pc2
-from geometry_msgs.msg import PointStamped
+from math import *
+from sensor_msgs.msg import LaserScan
+from bisect import bisect_left as lower_bound
+from std_msgs.msg import Float32MultiArray
+from lidar.msg import Obstacle, ObstacleArray
 
-def dbscan_clustering(pointcloud_msg,publish_topic):
-    pc_data = pc2.read_points(pointcloud_msg,field_names=("x","y","z"),skip_nans=True)
-    data= np.array(list(pc_data))
-    dbscan = DBSCAN(eps=0.2, min_samples=5)
-    labels = dbscan.fit_predict(data)
-    for label in set(labels) :
-        if label == -1:
-            continue
-        cluster_points = data[labels==label]
-        cluster_center = np.mean(cluster_points,axis=0)
+class LidarObstacle:
+    def __init__(self):
+        rospy.init_node("lidar_obstacle")
+        
+        self.scan_msg = LaserScan()
+        rospy.Subscriber("/lidar2D", LaserScan, self.callback)
 
-        if np.linalg.norm(cluster_center[:2])<10:
-            publish_cluster_center(cluster_center,publish_topic)
+        self.obstacle_pub = rospy.Publisher("/obstacle_info", ObstacleArray, queue_size = 10)
+        self.degrees = range(-180, 180)
 
-def publish_cluster_center(cluster_center,publish_topic):
-    center_msg = PointStamped()
-    center_msg.header.stamp =rospy.Time.now()
-    center_msg.point.x = cluster_center[0]
-    center_msg.point.y = cluster_center[1]
-    center_msg.point.z = cluster_center[2]
+    def callback(self, msg: LaserScan):
+        self.scan_msg = msg
+        is_searching_obstacle = False # 현재 장애물 인덱싱 중인가?
 
-    publisher.publish(center_msg)
+        obstacle_prev_deg = 0 
+        obstacle_start_deg = 0 # 임의의 장애물 첫 시작 인덱스
+
+        obst_size = 0
+        obstacle_arr = ObstacleArray()
+        ranges = self.scan_msg.ranges[:180][::-1] + self.scan_msg.ranges[180:][::-1]
+        
+        for index, value in enumerate(ranges):
+            if self.degrees[index] < -90 or self.degrees[index] > 90: continue
+
+            # 3.5 이내로 들어오는 경우
+            if 0 <= value <= 3.5:
+                if not is_searching_obstacle:  # 처음 장애물 인덱스 판단을 실시할 때                    
+                    # 시작했으니 1로 변경
+                    is_searching_obstacle = True
+                    obst_size = 0
+
+                    obstacle_prev_deg = self.degrees[index]
+                    obstacle_start_deg = self.degrees[index]
+                    
+                # 장애물 찾는 중
+                elif abs(self.degrees[index] - obstacle_prev_deg) < 8:
+                    obst_size += 1
+                    obstacle_prev_deg = self.degrees[index]
+            
+            elif is_searching_obstacle:
+                # 장애물의 크기가 너무 작으면(3보다 작은 경우) skip
+                if obst_size < 3:
+                    obst_size = 0
+                    is_searching_obstacle = False
+
+                else:
+                    middle_index = (obstacle_start_deg + obstacle_prev_deg) / 2
+                    middle_value = ranges[min(lower_bound(self.degrees, int(middle_index)), 359)]
+
+                    x = middle_value * sin(middle_index * pi / 180)
+                    y = middle_value * cos(middle_index * pi / 180)
+                    
+                    obstacle_arr.obstacle_infos.append(Obstacle(obst_x = x, obst_y = y))
+
+                    is_searching_obstacle = False
+
+        self.obstacle_pub.publish(obstacle_arr)
 
 def main():
-    rospy.init_node('obstacle_detection_node')
-    lidar_topic = '/lidar2D'
-    rospy.Subscriber(lidar_topic,PointCloud2,dbscan_clustering,callback_args='/obstacle_center')
-    global publisher
-    publisher = rospy.Publisher('obstacle_center',PointStamped,queue_size=10)
-    
-    rospy.spin()
+    try:
+        _ = LidarObstacle()
+        rospy.spin()
+        
+    except rospy.ROSInterruptException:
+        pass
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
