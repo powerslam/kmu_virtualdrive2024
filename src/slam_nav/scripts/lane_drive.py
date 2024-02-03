1a1976905d4c45ca9421d288b6601f5f479291b6
powerslam <timsort@kookmin.ac.kr>
2024-02-04 04:23:06 +0900

    얼추됨

--- src/slam_nav/scripts/lane_drive.py
+++ src/slam_nav/scripts/lane_drive.py
@@ -32,6 +32,25 @@ from obstacle_detect.msg import ObstacleInfo, ObstacleInfoArray
 from obstacle_detect.msg import LidarObstacleInfo, LidarObstacleInfoArray, RotaryArray
 from actionlib_msgs.msg import GoalStatus
 
+# 일단 amcl_pose 로 이동하다가
+# grad가 일정 이하로 떨어지면
+# 그 때 amcl_pose 갱신?
+
+# 그리고 grad가 일정 이하로 떨어진 그 때 한 순간은
+# 다시 grad가 복구될 때까지 "차선으로만" 주행
+
+# 복구가 된다면 조정된 amcl_pose 가지고 주행
+
+# 이걸 반복
+
+# amcl_pose 수정하잖아
+# sequence 이런거 
+
+# 기본적으로 오른쪽 차선에 대한 값만 가지고 주행을 하는 걸로
+# 그러다가 정지선을 뭐 4번 봤어 그 상태에서 왼쪽 차선이 사라지면?
+# 왼쪽 차선이 보일때까지 왼쪽으로 꺾고
+# 로터리 전까지 움직임
+
 class Total:
     def __init__(self):
         rospy.init_node("navigation_client")
@@ -41,65 +60,146 @@ class Total:
         
         self.stop_data = Float64(data=0)
 
-        self.target_vel = 1200
+        self.stop_flag = False
+        self.avoid_flag = False
+
+        self.NORMAL_VEL = 1800
+        self.AVOID_VEL = 1200
+
+        self.target_vel = self.NORMAL_VEL
         self.L_curv, self.R_curv, self.L_point, self.R_point = 0, 0, [], []
         rospy.Subscriber("/lane_information", LaneInformation, self.lane_callback)
 
-        self.prev_stop_lane_flag = False
-        self.stop_lane_flag = False
+        self.COUNT = 3
+
+        self.obstacle_judge_cnt = 0
+        self.obstacle_type = None
+        self.prev_obstacle_pt = None
+        self.obstacle_type = None
+        rospy.Subscriber('/lidar_obstacle_information', LidarObstacleInfoArray, self.mission1)
     
+    # 라이다 정보가 1초에 10번 들어옴
+    # 근데, 처리 시간이 있으니까 1초에 한 6 ~ 8번? 들어온다고 생각해야 함
+    def mission1(self, msg: LidarObstacleInfoArray):
+        obstacle_infos = msg.obstacle_infos
+        if not len(obstacle_infos):
+            self.avoid_flag = False
+            self.stop_flag = False
+
+            self.prev_obstacle_pt = None
+            self.obstacle_judge_cnt = 0
+            self.obstacle_type = None
+            return
+
+        # 나랑 가장 가까운 장애물을 가지고 판단
+        dists = np.array([np.hypot(info.obst_x, info.obst_y) for info in obstacle_infos])
+        chk_obstacle_idx = np.argmin(dists)
+
+        dist = max(0., dists[chk_obstacle_idx] - 0.21)
+        
+        # 거리가 가까우면 일단 정지함
+        # 장애물 타입이 결정되지 않은 경우
+        if not self.obstacle_type and dist < 0.8:
+            self.stop()
+            self.stop_flag = True
+            # print('hi?')
+
+            if self.prev_obstacle_pt is None:
+                self.prev_obstacle_pt = obstacle_infos[chk_obstacle_idx]
+
+            else:
+                self.obstacle_judge_cnt += 1
+                if self.obstacle_judge_cnt < 2: return
+
+                now_v = np.array([obstacle_infos[chk_obstacle_idx].obst_x, obstacle_infos[chk_obstacle_idx].obst_y])
+                now_norm = np.linalg.norm(now_v)
+                now_v /= now_norm
+                
+                prev_v = np.array([self.prev_obstacle_pt.obst_x, self.prev_obstacle_pt.obst_y])
+                prev_norm = np.linalg.norm(prev_v)
+                prev_v /= prev_norm
+                
+                angle = np.arccos(np.dot(now_v, prev_v))
+                print('각', angle)
+
+                self.obstacle_type = 's' if abs(angle) < 0.06 else 'd'
+
+        elif self.obstacle_type == 's': # 정적 장애물인 경우
+            print('정적')
+
+            self.stop_flag = True
+            self.stop()
+            return
+
+            # 특정 영역 밖이면 그냥 지나가기
+            if obstacle_infos[chk_obstacle_idx].obst_x < -0.2 or obstacle_infos[chk_obstacle_idx].obst_x > 0.2:
+                print('음?')
+                self.stop_flag = False
+                return
+            
+            else:
+                print('음??')
+                # 회피기동
+                pass
+        
+        elif self.obstacle_type == 'd': # 동적 장애물인 경우
+            print('동적')
+            # 장애물의 x 좌표가 0보다 큰 경우 ==> 차선을 탈출했다고 판정하고 출발
+            if obstacle_infos[chk_obstacle_idx].obst_x > 0:
+                self.stop_flag = False
+                return
+
+            if self.obstacle_judge_cnt == 2:
+                self.stop_flag = False
+                return
+            
+            if 0.1 < obstacle_infos[chk_obstacle_idx].obst_y:
+                self.stop_flag = False
+                return
+            
+            self.stop_flag = True
+            self.stop()
+
+                # # 재정지 해야하는 경우임
+                # # 장애물의 x 좌표가 -0.8 인 경우 그리고, 장애물의 좌표가 0.1 보다 큰 경우 거리가 아직 1.3 보다 작은 경우는 정지
+                # elif obstacle_infos[chk_obstacle_idx].obst_x > -0.8 and obstacle_infos[chk_obstacle_idx].obst_y > 0.1 and dist < 0.95:
+                #     self.stop_flag = True
+                #     self.stop()
+
+            # 지금은 정적 장애물의 경우에만 따지는 걸로
+            # 그리고 동적 장애물은 이전 위치와 현재 위치가 달라지면 알아보는 걸로
+            
+
     def lane_callback(self, msg: LaneInformation):
         self.L_curv = msg.left_gradient
         self.R_curv = msg.right_gradient
         self.L_point = msg.left_lane_points
         self.R_point = msg.right_lane_points
 
-        # 0.07 ~ 0.25 언저리가 나옴
-        # 0.07 일때 최대 조향 ==> 좌회전일 때 양수
-        # 좌회전인 경우
-        self.steer_angle = 0.5
-        # if 0 < self.R_curv < 0.1:
-        # self.target_vel = 2400
+        # 장애물 회피중이라 동작하면 안 됨
+        if not self.avoid_flag:
+            # 오른차선의 중앙값보다 r7_pt 의 좌표가 작으면 => 왼쪽으로 더 틀어야 함
+            # 오른차선의 중앙값보다 r7_pt 의 좌표가 크면 => 오른쪽으로 더 틀어야 함
+            
+            e = 0.00000000001
+            some_value = 1 / ((self.R_curv + e) * 2.5) # 살짝 키운값
 
-        # 기준이 
-        some_value = 1 / (self.R_curv * 2.5) # 살짝 키운값
+            MID_POINT = 342
 
-        MID_POINT = 342
+            l7_pt = self.L_point[7].x
+            r7_pt = self.R_point[7].x
 
-        l7_pt = self.L_point[7].x
-        r7_pt = self.R_point[7].x
+            m = (l7_pt + r7_pt) // 2
 
-        m = (l7_pt + r7_pt) // 2
+            # 1차 조향각
+            self.steer_angle = self.mapping(some_value, -20, 20, 1, 0)
 
-        # 1차 조향각
-        self.steer_angle = self.mapping(some_value, -20, 20, 1, 0)
+            # 오른차선의 중앙값보다 r7_pt 의 좌표가 작으면 => 왼쪽으로 더 틀어야 함
+            # 오른차선의 중앙값보다 r7_pt 의 좌표가 크면 => 오른쪽으로 더 틀어야 함
+            self.steer_angle -= (MID_POINT - m) / 1000
 
-        # 오른차선의 중앙값보다 r7_pt 의 좌표가 작으면 => 왼쪽으로 더 틀어야 함
-        # 오른차선의 중앙값보다 r7_pt 의 좌표가 크면 => 오른쪽으로 더 틀어야 함
-        self.steer_angle -= (MID_POINT - m) / 1000
+        # print(self.steer_angle)
 
-        print(self.steer_angle)
-
-            # self.mapping(self.R_curv, 0.07, 0.25, 0.5, 0)
-
-        # 일단 amcl_pose 로 이동하다가
-        # grad가 일정 이하로 떨어지면
-        # 그 때 amcl_pose 갱신?
-
-        # 그리고 grad가 일정 이하로 떨어진 그 때 한 순간은
-        # 다시 grad가 복구될 때까지 "차선으로만" 주행
-        
-        # 복구가 된다면 조정된 amcl_pose 가지고 주행
-
-        # 이걸 반복
-
-        # amcl_pose 수정하잖아
-        # sequence 이런거 
-
-        # 기본적으로 오른쪽 차선에 대한 값만 가지고 주행을 하는 걸로
-        # 그러다가 정지선을 뭐 4번 봤어 그 상태에서 왼쪽 차선이 사라지면?
-        # 왼쪽 차선이 보일때까지 왼쪽으로 꺾고
-        # 로터리 전까지 움직임
         self.run()
 
     def create_trajectory(self, tgt_x, tgt_y, dist):
@@ -167,8 +267,12 @@ class Total:
         plt.show()
 
     def run(self):
-        self.vel_pub.publish(self.target_vel)
-        self.steer_pub.publish(self.steer_angle)
+        # 항상 정지
+        if self.stop_flag: return
+        
+        # print('동작 중', self.steer_angle)
+        self.vel_pub.publish(Float64(data=self.target_vel))
+        self.steer_pub.publish(Float64(data=self.steer_angle))
 
     def mapping(self, value, from_min, from_max, to_min, to_max):
         value = np.clip(value, from_min, from_max)
