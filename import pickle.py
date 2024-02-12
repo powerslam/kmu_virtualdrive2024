3d97cf86c26e872465c5808ce882b7629ce87653
timsort <timsort@kookmin.ac.kr>
2024-02-13 03:02:10 +0900

    필요없는 파일 삭제

--- import pickle.py
+++
@@ -1,8 +0,0 @@
-import rosbag
-
-bag = rosbag.Bag('2024-02-04-08-01-17.bag')
-
-for topic, msg, t in bag.read_messages():
-    print(f"Topic: {topic}, Message: {msg}, Timestamp: {t}")
-
-bag.close()
